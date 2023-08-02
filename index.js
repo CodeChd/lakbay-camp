0de101ef584ad2ec5c5d7846ed3e1ab37f39fc93
CodeChd <Drowzyking1126@gmail.com>
2023-08-02 09:56:14 +0800

    LakbayCamp -f

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const Campground = require('./models/Campground')
+
+const mongoose = require('mongoose')
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/lakbay-camp")
+  .then(() => console.log("CONNECTION OPEN!!"))
+  .catch((err) => {
+    console.log(err, "Oops error");
+  });
+
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'content'))
+
+app.get('/' ,(req, res) => {
+    res.render('home')
+})
+
+app.get('makecampground', (req, res) => {
+    
+
+})
+
+app.listen(3000, () => {
+    console.log('LISTENING TO PORT 3000')
+})
