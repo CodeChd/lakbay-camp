9d7661f88651b300c3fa1e97203b9784c10d0540
CodeChd <Drowzyking1126@gmail.com>
2023-08-09 20:21:59 +0800

    Campground Adding Basic Style

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejsMate = require('ejs-mate')
 const methodOverride = require("method-override");
 
 const Campground = require("./models/Campground");
@@ -14,6 +15,8 @@ mongoose
     console.log(err, "Oops error");
   });
 
+
+app.engine('ejs', ejsMate)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "content"));
 
