eeb40fbede535b9830ad9895daacbd8f8922e8a1
CodeChd <Drowzyking1126@gmail.com>
2023-08-24 21:35:39 +0800

    Delete Campground  Associate Reviews

--- models/Campground.js
+++ models/Campground.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const { campgroundSchema } = require("../schema");
 const Schema = mongoose.Schema;
+const Reviews = require("./review");
 
 const CampgroundSchema = new Schema({
   title: String,
@@ -7,9 +9,13 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
-  reviews: [
-    {type: Schema.Types.ObjectId, ref: 'Review'}
-  ]
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+});
+
+CampgroundSchema.post("findOneAndDelete", async (doc) => {
+  if (doc) {
+    await Reviews.deleteMany({ _id: { $in: doc.reviews } });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
